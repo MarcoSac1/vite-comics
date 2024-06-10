f4819e61da61c22ae623d309c1bf738ed99a1e2f
MarcoSac1 <marcosaccone8@gmail.com>
2024-06-10 15:35:44 +0200

    loaded img folder created Header, Main, Footer component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
-<script setup>
+<script>
+export default{
+  data(){
+
+  }
+  components:{
+    AppHeader,
+    AppMain,
+    AppFooter,
+  }
+}
+
+import AppFooter from './components/AppFooter.vue';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 
 </script>
 
 <template>
 
+<AppHeader/>
+<AppMain/>
+<AppFooter/>
+
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@use './style/general.scss';
+@use './style/partials/mixins';
+@use './style/partials/variable';
 </style>
