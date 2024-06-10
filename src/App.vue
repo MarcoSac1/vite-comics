dc0bca5b069e3208c288d373959a66ce30ef7905
MarcoSac1 <marcosaccone8@gmail.com>
2024-06-10 15:58:04 +0200

    gived brand color style

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <script>
-export default{
-  data(){
+  export default {
+    data(){
 
+    },
+    components:{
+      AppHeader,
+      AppMain,
+      AppFooter
+    }
   }
-  components:{
-    AppHeader,
-    AppMain,
-    AppFooter,
-  }
-}
-
-import AppFooter from './components/AppFooter.vue';
-import AppHeader from './components/AppHeader.vue';
-import AppMain from './components/AppMain.vue';
 
 </script>
 
